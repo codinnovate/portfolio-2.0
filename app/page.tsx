eb53c1b9134915c0a0fb8bc95abbf814d347de63
codinnovate <adeyemis710@gmail.com>
2025-07-25 13:51:58 -0700

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AboutMe from './_components/AboutMe';
 import Banner from './_components/Banner';
 import Experiences from './_components/Experiences';
 import Skills from './_components/Skills';
-import ProjectList from './_components/ProjectList';
+// import ProjectList from './_components/ProjectList';
 
 export default function Home() {
     return (
